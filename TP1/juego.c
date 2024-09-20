215f08c055a8daf23245a4e8d1c4a77db5e21c51
AvrilMZ <amorfeo@fi.uba.ar>
2024-09-20 00:17:05 -0300

    Detalles base de estructura antes de comenzar el codigo.

--- TP1/juego.c
+++ TP1/juego.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "restaurant.h"
 
 int main(){
-    srand (( unsigned)time(NULL)); //Por consigna solo debe estar 1 vez
+    srand ((unsigned)time(NULL)); //Por consigna solo debe estar 1 vez
 
     return 0;
 }
