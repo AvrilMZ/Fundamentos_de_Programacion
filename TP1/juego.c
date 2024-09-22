cc2231754d9ba087d42c9e5104353147cb388bbf
Avril <amorfeo@fi.uba.ar>
2024-09-21 22:53:34 -0300

    Archivo destinado a realizar pruebas de fragmentos del codigo

--- TP1/juego.c
+++ TP1/juego.c
@@ -4,7 +4,26 @@
 #include "restaurant.h"
 
 int main(){
-    srand ((unsigned)time(NULL)); //Por consigna solo debe estar 1 vez
+    srand((unsigned)time(NULL));
+    juego_t *juego;
+    inicializar_juego(juego);
+
+    while (true){
+        char accion = ' ';
+        mostrar_juego(*juego);
+        printf("Utilice W, S, A, D para moverse ó la O para interactuar con la mopa:\n");
+        scanf(" %c", &accion);
+        realizar_jugada(juego, accion);
+        
+        int estado = estado_juego(*juego);
+        if (estado == 1){
+            printf("¡Ganaste!");
+            return false;
+        } else if (estado == -1){
+            printf("Perdiste");
+            return false;
+        }
+    }
 
     return 0;
 }
