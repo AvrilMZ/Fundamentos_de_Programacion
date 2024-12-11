09d3aa46c5ebe1452d8524d7668afae4f0499610
AvrilMZ <amorfeo@fi.uba.ar>
2024-12-11 01:30:16 -0300

    Codigo actualizado

--- TP0/ratatouille.c
+++ TP0/ratatouille.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
-
 #define MILANESA_NAPOLITANA 'M'
 #define HAMBURGUESA 'H'
 #define PARRILLA 'P'
@@ -22,88 +20,96 @@ const int PRECIO_AGUA = 2005;
 const int MAXIMO_COMENSALES = 5;
 const int PORCENTAJE_PROPINA = 10;
 
-
-
 //PRE: -
 //POST: La función devuelve true si el carácter representa una comida válida, de lo contrario devuelve false.
-bool comidas_validas(char comida) {
-	if ((comida == MILANESA_NAPOLITANA) || (comida == HAMBURGUESA) || (comida == PARRILLA) || (comida == RATATOUILLE)){
-        return true;
-    } else return false;
+bool es_comida_valida(char comida) {
+	return ((comida == MILANESA_NAPOLITANA) || (comida == HAMBURGUESA) || (comida == PARRILLA) || (comida == RATATOUILLE));
 }
 
 //PRE: -
 //POST: La función devuelve true si el carácter representa una bebida válida, de lo contrario devuelve false.
-bool bebidas_validas(char bebida) {
-	if ((bebida == AGUA) || (bebida == GASEOSA) || (bebida == JUGO)){
-        return true;
-    } else return false;
+bool es_bebida_valida(char bebida) {
+	return ((bebida == AGUA) || (bebida == GASEOSA) || (bebida == JUGO));
+}
+
+//PRE: El puntero 'cantidad_comensales' debe estar previamente inicializado.
+//POST: Actualiza el valor apuntado por el puntero 'cantidad_comensales' con una cantidad válida de comensales.
+void eleccion_cantidad_comensales(int* cantidad_comensales) {
+    printf("Cuántos comensales van a ser? (Ingrese un valor entre 1 y %i)\n", MAXIMO_COMENSALES);
+    scanf("%i", cantidad_comensales);
+
+    while (*cantidad_comensales < 1 || *cantidad_comensales > MAXIMO_COMENSALES) {
+        printf("La cantidad ingresada no está dentro de nuestro rango. ¿Cuántos comensales van a ser? (Ingrese un valor entre 1 y %i)\n", MAXIMO_COMENSALES);
+        scanf("%i", cantidad_comensales);
+    }
 }
 
 //PRE: -
 //POST: Devuelve el costo base total según la cantidad de comensales proporcionada.
-int calculo_base(){
+int calcular_base(){
     int cantidad_comensales = -1;
-    printf("Cuántos comensales van a ser? (Ingrese un valor entre 1 y %i)\n", MAXIMO_COMENSALES);
-    scanf("%i", &cantidad_comensales);
+    eleccion_cantidad_comensales(&cantidad_comensales);
 
-    while (cantidad_comensales < 1 || cantidad_comensales > MAXIMO_COMENSALES){
-        printf("La cantidad ingresada no está dentro de nuestro rango. ¿Cuántos comensales van a ser? (Ingrese un valor entre 1 y %i)\n", MAXIMO_COMENSALES);
-        scanf("%i", &cantidad_comensales);
-    }
-    
-    int total_base = cantidad_comensales * PRECIO_BASE;
-    return total_base;
+    return cantidad_comensales * PRECIO_BASE;
 }
 
-//PRE: "cantidad_comensales" debe ser un valor positivo.
-//POST: Devuelve el costo total de la comida según la opción elegida y la cantidad de comensales.
-int calculo_comida(int cantidad_comensales){
-    char comida_elegida = 'Z';
-    printf("¿Qué desean comer? (Ingrese M, H, P ó R)\n");
-    scanf(" %c", &comida_elegida);
+//PRE: El puntero 'comida_electa' debe estar previamente inicializado.
+//POST: Actualiza el valor apuntado por el puntero 'comida_electa' con una elección válida de comida.
+void eleccion_comida(char* comida_electa) {
+    printf("¿Qué desean comer? (Ingrese %c, %c, %c o %c)\n", MILANESA_NAPOLITANA, HAMBURGUESA, PARRILLA, RATATOUILLE);
+    scanf(" %c", comida_electa);
     
-    while (comidas_validas(comida_elegida) == false){
-        printf("El carácter ingresado no está en nuestro menú. ¿Qué desean comer? (Ingrese M, H, P ó R)\n");
-        scanf(" %c", &comida_elegida);
+    while (!es_comida_valida(*comida_electa)) {
+        printf("El carácter ingresado no está en nuestro menú. ¿Qué desean comer? (Ingrese %c, %c, %c o %c)\n", MILANESA_NAPOLITANA, HAMBURGUESA, PARRILLA, RATATOUILLE);
+        scanf(" %c", comida_electa);
     }
+}
+
+//PRE: 'cantidad_comensales' debe ser un valor positivo.
+//POST: Devuelve el costo total de la comida según la opción electa y la cantidad de comensales.
+int calcular_comida(int cantidad_comensales) {
+    char comida_electa = ' ';
+    eleccion_comida(&comida_electa);
 
     int total_comida = 0;
-    switch (comida_elegida){
+    switch (comida_electa) {
         case MILANESA_NAPOLITANA:
             total_comida += PRECIO_MILANESA_NAPOLITANA;
-        break;
-
+            break;
         case HAMBURGUESA:
             total_comida += PRECIO_HAMBURGUESA;
-        break;
-
+            break;
         case PARRILLA:
             total_comida += PRECIO_PARRILLA;
-        break;
-
+            break;
         case RATATOUILLE:
             total_comida += PRECIO_RATATOUILLE;
-        break;
+            break;
     }
 
     return total_comida * cantidad_comensales;
 }
 
-//PRE: "cantidad_comensales" debe ser un valor positivo.
-//POST: Devuelve el costo total de la bebida según la opción elegida y la cantidad de comensales.
-int calculo_bebida(int cantidad_comensales){
-    char bebida_elegida = 'Z';
-    printf("¿Qué desean beber? (Ingrese A, G, ó J)\n");
-    scanf(" %c", &bebida_elegida);
+//PRE: El puntero 'bebida_electa' debe estar previamente inicializado.
+//POST: Actualiza el valor apuntado por el puntero 'bebida_electa' con una elección válida de bebida.
+void eleccion_bebida(char* bebida_electa) {
+    printf("¿Qué desean beber? (Ingrese %c, %c, o %c)\n", AGUA, GASEOSA, JUGO);
+    scanf(" %c", bebida_electa);
 
-    while (bebidas_validas(bebida_elegida) == false){
-        printf("El carácter ingresado no está en nuestro menú. ¿Qué desean beber? (Ingrese A, G, ó J)\n");
-        scanf(" %c", &bebida_elegida);
+    while (!es_bebida_valida(*bebida_electa)) {
+        printf("El carácter ingresado no está en nuestro menú. ¿Qué desean beber? (Ingrese %c, %c, o %c)\n", AGUA, GASEOSA, JUGO);
+        scanf(" %c", bebida_electa);
     }
+}
+
+//PRE: 'cantidad_comensales' debe ser un valor positivo.
+//POST: Devuelve el costo total de la bebida según la opción electa y la cantidad de comensales.
+int calcular_bebida(int cantidad_comensales) {
+    char bebida_electa = ' ';
+    eleccion_bebida(&bebida_electa);
 
     int total_bebida = 0;
-    switch (bebida_elegida){
+    switch (bebida_electa) {
         case AGUA:
             total_bebida += PRECIO_AGUA;
         break;
@@ -120,31 +126,27 @@ int calculo_bebida(int cantidad_comensales){
     return total_bebida * cantidad_comensales;
 }
 
-//PRE: "total_base", "total_comida" y "total_bebida" deben ser valores positivos.
+//PRE: -
 //POST: Devuelve el monto total de la propina, calculado como un porcentaje específico del total final.
-int calculo_propina(int total_base, int total_comida, int total_bebida){
-    int total_final = total_base + total_comida + total_bebida;
-    int total_propina = (total_final * PORCENTAJE_PROPINA) / 100;
-    return total_propina;
+int calcular_propina(int total_base, int total_comida, int total_bebida) {
+    return ((total_base + total_comida + total_bebida) * PORCENTAJE_PROPINA) / 100;
 }
 
 //PRE: -
 //POST: Imprime el costo total de la cuenta, incluyendo un detallado de los gastos y la propina.
-void calculo_total(int total_base, int total_comida, int total_bebida, int total_propina){
+void calcular_total(int total_base, int total_comida, int total_bebida, int total_propina) {
     int precio_final = total_base + total_comida + total_bebida + total_propina;
-    printf("Con los platos ($%i), las bebidas solicitadas ($%i) y la propina incluida de un %i%c ($%i) el total de su cuenta es: -$%i-.",
+    printf("Con los platos ($%i), las bebidas solicitadas ($%i) y la propina incluida de un %i%c ($%i) el total de su cuenta es: -$%i-\n",
     total_base + total_comida, total_bebida, PORCENTAJE_PROPINA, '%', total_propina, precio_final);
 }
 
-
-
-int main(){
-    int base = calculo_base();
+int main() {
+    int base = calcular_base();
     int cantidad_comensales = base / PRECIO_BASE;
-    int comida = calculo_comida(cantidad_comensales);
-    int bebida = calculo_bebida(cantidad_comensales);
-    int propina = calculo_propina(base, comida, bebida);
-    calculo_total(base, comida, bebida, propina);
+    int comida = calcular_comida(cantidad_comensales);
+    int bebida = calcular_bebida(cantidad_comensales);
+    int propina = calcular_propina(base, comida, bebida);
+    calcular_total(base, comida, bebida, propina);
 
     return 0;
 }
