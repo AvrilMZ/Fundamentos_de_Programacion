26570d2f18670500d1968a2e297835e68c467264
AvrilMZ <amorfeo@fi.uba.ar>
2025-01-26 18:54:37 -0300

    Resuelto

--- Actividades/Archivos - Ejercicios Extra/Ejercicio 11.13.1.py
+++ Actividades/Archivos - Ejercicios Extra/Ejercicio 11.13.1.py
@@ -1,8 +1,10 @@
 '''Escribir una función, llamada head que reciba un archivo y un número N e imprima las primeras N líneas del archivo.'''
 
+MODO_LECTURA = 'r'
+
 def head(nombre_archivo, cantidad_lineas):
     lineas_leidas = []
-    with open(nombre_archivo, "r") as archivo: 
+    with open(nombre_archivo, MODO_LECTURA) as archivo: 
         for i in range(cantidad_lineas):
             lineas_leidas.append(archivo.readline())
     print(lineas_leidas)
