bed344809c2f16dc72cf6b4cc83f24f27c94627d
AvrilMZ <amorfeo@fi.uba.ar>
2025-02-02 20:27:29 -0300

    Resuelto

--- Actividades/Clases Practicas/Resoluciones/Clase_14_Ej1.py
+++ Actividades/Clases Practicas/Resoluciones/Clase_14_Ej1.py
@@ -5,9 +5,11 @@ ARCHIVO_AUX = "auxiliar_discurso.txt"
 MODO_LECTURA = "r"
 MODO_ESCRITURA = "w"
 
-def cambiar_discurso():
+# PRE: 'file_discurso' debe existir.
+# POST: Devuelve un archivo con el mismo contenido unicamente que con las palabras "Hallie", "California" y "papá" reemplazadas.
+def cambiar_discurso(file_discurso):
     try:
-        with open(ARCHIVO_DISCURSO, MODO_LECTURA) as discurso, open(ARCHIVO_AUX, MODO_ESCRITURA) as copia:
+        with open(file_discurso, MODO_LECTURA) as discurso, open(ARCHIVO_AUX, MODO_ESCRITURA) as copia:
             linea = discurso.readline()
             while linea:
                 linea = linea.replace("Hallie", "Annie")
@@ -16,9 +18,9 @@ def cambiar_discurso():
                 copia.write(linea)
                 linea = discurso.readline()
         
-        os.rename(ARCHIVO_AUX, ARCHIVO_DISCURSO)
+        os.rename(ARCHIVO_AUX, file_discurso)
     except:
-        print(f"Error al abrir el archivo {ARCHIVO_DISCURSO}.")
+        print(f"Error al abrir el archivo {file_discurso}.")
         return
 
 
